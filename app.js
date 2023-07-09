3c9e2eff05b9e93e339862f9af81a25b6df0c547
BizarrIlliada <kazkoviy@icloud.com>
2023-07-09 17:07:08 +0300

    rendering list items

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      goals: ['Illia', 'Boiko', 'HH', '!@#', '123'],
+      newGoal: '',
+      goalToDelete: '',
+      me: {
+        name: 'Illia',
+        'last name': 'Boiko',
+        age: 22,
+        city: 'Lviv'
+      }
+    };
   },
+
+  methods: {
+    addGoal() {
+      this.goals.push(this.newGoal);
+      this.newGoal = '';
+    },
+
+    deleteGoal(index) {
+      this.goals.splice(index, 1)
+    }
+  },
+
+  watch: {
+    goals: {
+      handler(value) {
+        if (value.length > 5) {
+          value.shift();
+        }
+      },
+
+      deep: true,
+    }
+  }
 });
 
 app.mount('#user-goals');
